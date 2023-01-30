29652c0b583a1be31a4ca644e00403dde0d98b10
Abdul Rafay <abdulrafay141099@gmail.com>
2023-01-30 21:19:03 +0500

    initial commit

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Parking_App.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
+
