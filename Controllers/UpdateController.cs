dcdb0c8bf97b2b1bd158b39adcee438b0d9a954c
Sumair Hassan <86984915+sumairhw@users.noreply.github.com>
2023-02-06 00:03:08 +0500

    structure of profile page and home page, made a separate layout

---
+++ Controllers/UpdateController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Parking_App.Controllers;
+
+public class UpdateController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
